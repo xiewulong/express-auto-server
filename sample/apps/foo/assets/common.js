d23ef46c09fe5281bf944d8646f69e7bcc50dd44
xiewulong <xiewulong@vip.qq.com>
2017-05-08 18:18:34 +0800

    express auto server

---
+++ sample/apps/foo/assets/common.js
@@ -0,0 +1,17 @@
+/*!
+ * common asset
+ * xiewulong <xiewulong@vip.qq.com>
+ * create: 2017/05/08
+ * since: 0.0.1
+ */
+'use strict';
+
+module.exports = {
+	name: 'common',
+	production: true,
+	// basePath: '@webroot',
+	// baseUrl: '@web',
+	css: true,
+	js: true,
+	depends: '@common/assets/jquery',
+};
---
+++ sample/common/assets/jquery.js
@@ -0,0 +1,14 @@
+/*!
+ * jquery asset
+ * xiewulong <xiewulong@vip.qq.com>
+ * create: 2017/05/08
+ * since: 0.0.1
+ */
+'use strict';
+
+module.exports = {
+	name: 'jquery',
+	production: true,
+	sourcePath: '@npm/jquery/dist',
+	js: true,
+};
